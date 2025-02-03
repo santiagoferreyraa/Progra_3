d95254f49c75ed3e7614e3ed923e9db15bf6ab1c
AgustinCervellini <67561081+AgustinCervellini@users.noreply.github.com>
2025-02-02 23:35:09 -0300

    Delete Ejercicios/source/Clase1/Actividad5 directory

--- Ejercicios/source/Clase1/Actividad5/Vehiculo.java
+++
@@ -1,9 +0,0 @@
-package Actividad5;
-
-public abstract class Vehiculo {
-    protected int numeroDeMatricula;
-    protected String marca;
-    protected String modelo;
-
-    public abstract void MostrarInformacion();
-}
