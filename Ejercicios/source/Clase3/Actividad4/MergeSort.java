4a3988501456daa200ca051001b8663fdde66126
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-02-09 12:43:14 -0300

    Fix Clase 3

--- Ejercicios/source/Clase3/Actividad4/MergeSort.java
+++ Ejercicios/source/Clase3/Actividad4/MergeSort.java
@@ -4,11 +4,9 @@ package Ejercicios.source.Clase3.Actividad4;
 
 public class MergeSort {
     
-    // Método para mezclar dos mitades ordenadas
     private static void merge(int[] arreglo, int[] izquierda, int[] derecha) {
         int i = 0, j = 0, k = 0;
 
-        // Mezclar los elementos en orden
         while (i < izquierda.length && j < derecha.length) {
             if (izquierda[i] <= derecha[j]) {
                 arreglo[k++] = izquierda[i++];
@@ -17,41 +15,32 @@ public class MergeSort {
             }
         }
 
-        // Copiar los elementos restantes de la mitad izquierda
         while (i < izquierda.length) {
             arreglo[k++] = izquierda[i++];
         }
 
-        // Copiar los elementos restantes de la mitad derecha
         while (j < derecha.length) {
             arreglo[k++] = derecha[j++];
         }
     }
     
-    // Método principal que ordena el arreglo
+    
     public static void mergeSort(int[] arreglo) {
         if (arreglo.length < 2) {
-            return; // Si el arreglo tiene 0 o 1 elementos, ya está ordenado
+            return; 
         }
         int medio = arreglo.length / 2;
-
-        // Dividir el arreglo en dos mitades
         int[] izquierda = new int[medio];
         int[] derecha = new int[arreglo.length - medio];
 
-        // Copiar los elementos a las mitades
         for (int i = 0; i < medio; i++) {
             izquierda[i] = arreglo[i];
         }
         for (int i = medio; i < arreglo.length; i++) {
             derecha[i - medio] = arreglo[i];
         }
-
-        // Llamadas recursivas para ordenar las mitades
         mergeSort(izquierda);
         mergeSort(derecha);
-
-        // Mezclar las mitades ordenadas
         merge(arreglo, izquierda, derecha);
     }
 
