75cb1b07543cf3ea49ff4cbfe3f263888b423a0e
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-02-24 14:19:16 -0300

    Clase14

---
+++ Ejercicios/source/Clase14/SudokuSolver6x6.java
@@ -0,0 +1,82 @@
+package Ejercicios.source.Clase14;
+
+public class SudokuSolver6x6 {
+
+    private static final int SIZE = 6;
+    public static void main(String[] args) {
+        int[][] board = {
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No existe solución para este Sudoku.");
+        }
+    }
+
+    // Método que resuelve el Sudoku usando backtracking
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 6; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int startRow = row - row % 2;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
